36f8e4ff339046cf235c74f34e0dae80e3e6d80b
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2023-04-24 17:15:27 +0200

    Add netX500/100 XM2_IOs and XM3_IOs to the list of known pins.

--- iomatrix/templates/io_matrix/netx500.lua
+++ iomatrix/templates/io_matrix/netx500.lua
@@ -145,6 +145,10 @@ IoMatrix_netx500.atKnownPins = {
   ['XM0_IO1'] = { type=${PINTYPE_XMIO}, index=1 },
   ['XM1_IO0'] = { type=${PINTYPE_XMIO}, index=2 },
   ['XM1_IO1'] = { type=${PINTYPE_XMIO}, index=3 },
+  ['XM2_IO0'] = { type=${PINTYPE_XMIO}, index=4 },
+  ['XM2_IO1'] = { type=${PINTYPE_XMIO}, index=5 },
+  ['XM3_IO0'] = { type=${PINTYPE_XMIO}, index=6 },
+  ['XM3_IO1'] = { type=${PINTYPE_XMIO}, index=7 }
 }
 
 return IoMatrix_netx500
