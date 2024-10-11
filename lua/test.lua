e304e38b0ba206a967c96fd03f79a2552073871c
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2024-10-11 16:08:27 +0200

    Split long lines.

--- lua/test.lua
+++ lua/test.lua
@@ -1,2 +1,4 @@
 local TestClassIoMatrix = require 'test_class_iomatrix'
-return function(ulTestID, tLogWriter, strLogLevel) return TestClassIoMatrix('@NAME@', ulTestID, tLogWriter, strLogLevel) end
+return function(ulTestID, tLogWriter, strLogLevel)
+  return TestClassIoMatrix('@NAME@', ulTestID, tLogWriter, strLogLevel)
+end
